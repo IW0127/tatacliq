9b9a02fe50a7dff4f6d871d097581b9adc3aa088
keyur navadiya <iw0098>
2022-05-13 18:43:48 +0530

    slide

--- script.js
+++ script.js
@@ -1,48 +1,20 @@
-const outerContentSlide = document.querySelector(".img-slide");
-const contentImages = document.querySelectorAll(".img-slide img");
+$(document).ready(function () {
+    $(".subCategory").hide();
+    $(".category").mouseenter(function () {
+        let id = $(this).data('id');
+        i = $("#" + id).show();
+    });
+    $(".category").mouseleave(function () {
+        $(".subCategory").hide();
+    });
 
-const prevBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
+    $(".catBrad").mouseenter(function () {
+        let id = $(this).data('id');
+        i = $("#" + id).show();
+    });
 
-let counter = 1;
-const size = contentImages[0].clientWidth;
-
-outerContentSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-const next = () => {
-    if (counter >= contentImages.length - 1) return;
-    outerContentSlide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    outerContentSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-}
-const prev = () => {
-    if (counter <= 0) return;
-    outerContentSlide.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-    outerContentSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-}
-nextBtn.addEventListener('click', () => {
-    next();
-});
-prevBtn.addEventListener('click', () => {
-    prev();
+    $(".subCategory").mouseenter(function () {
+        $(this).show();
+    });
 });
 
-outerContentSlide.addEventListener('transitionend', () => {
-    if (contentImages[counter].id === 'lastClone') {
-        outerContentSlide.style.transition = "none";
-        counter = contentImages.length - 2;
-        outerContentSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-    }
-    if (contentImages[counter].id === 'firstClone') {
-        outerContentSlide.style.transition = "none";
-        counter = contentImages.length - counter;
-        outerContentSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-    }
-});
-
-setInterval(() => {
-    next();
-}, 3000);
